2d99d4008906ee805722716afc1068eebe556cac
Isashi <isashimitsui@gmail.com>
2017-07-12 20:18:50 +0200

    final

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require "sinatra"
+require_relative "db/models"
 
-get "/" do 
-	erb :index, layout: :main
+get "/" do
+  @fishes = Fish.all
+  erb :index, layout: :main
+end
+
+get "/:fishname" do
+  @fish = Fish.find_by(name: params[:fishname])
+  erb :show, layout: :main
+end
+
+post "/" do
+  Fish.create!(params)
+  redirect "/"
 end
